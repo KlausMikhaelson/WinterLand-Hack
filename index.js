b118a0aab646e8870b1cbd85fbb6f8db5eb568b3
KlausMikhaelson <satyamsingh5076@gmail.com>
2022-12-25 01:46:28 +0530

    dk what I did

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ class Player {
     }
     update() {
         this.draw()
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
         if(this.position.y + this.height + this.velocity.y <= canvas.height ) {
             this.velocity.y += gravity
@@ -36,12 +37,27 @@ class Player {
 }
 
 const player = new Player()
+const keys = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    }
+}
 
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0,0, canvas.width, canvas.height)
     console.log("Yo")
 player.update()
+if(keys.right.pressed) {
+    player.velocity.x = 5
+} else if(keys.left.pressed) {
+    player.velocity.x = -5
+} else {
+    player.velocity.x = 0
+}
 }
 
 animate()
@@ -50,17 +66,43 @@ addEventListener('keydown', ({keyCode}) => {
     switch(keyCode) {
         case 87:
             console.log('up')
-            player.velocity.y += 20
+            player.velocity.y -= 20 
             break;
         case 65:
             console.log("left")
+            keys.left.pressed = true
             break;
         case 68:
             console.log("right")
+            keys.right.pressed = true
             break;
         case 83:
             console.log("down")
     }
+    console.log(keys.right.pressed)
+    console.log(keys.left.pressed)
+}) 
+
+addEventListener('keyup', ({keyCode}) => {
+    switch(keyCode) {
+        case 87:
+            console.log('up')
+            // player.velocity.y -= 20 
+            break;
+        case 65:
+            console.log("left")
+            keys.left.pressed = false
+            break;
+        case 68:
+            console.log("right")
+            keys.right.pressed = false
+            break;
+        case 83:
+            console.log("down")
+            break;
+    }
+    console.log(keys.right.pressed)
+    console.log(keys.left.pressed)
 }) 
 
 // w = 87, a = 65, s = 83, d = 68
