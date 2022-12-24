39c2b443ac3d5ec6a648b75cce638bff36577081
Satyam <100528412+KlausMikhaelson@users.noreply.github.com>
2022-12-24 21:48:33 +0000

    added scroll background

--- index.js
+++ index.js
@@ -36,10 +36,10 @@ class Player {
     } 
 }
 class Platform {
-  constructor() {
+  constructor({x, y}) {
     this.position = {
-      x: 200,
-      y: 200
+      x,
+      y
     }
     this.height = 20
     this.width = 200
@@ -50,8 +50,7 @@ class Platform {
   }
 }
 const player = new Player()
-const platform = new Platform()
-// const platforms = [new Platform()]
+const platforms = [new Platform({x: 200,y: 100}), new Platform({x:500, y: 200})]
 const keys = {
     right: {
         pressed: false
@@ -66,7 +65,10 @@ function animate() {
     c.clearRect(0,0, canvas.width, canvas.height)
     console.log("Yo")
 player.update()
-platform.draw()
+platforms.forEach((platform) => {
+  platform.draw()
+})
+
 if(keys.right.pressed && player.position.x < 400) {
     player.velocity.x = 5
 } else if(keys.left.pressed && player.position.x > 100) {
@@ -75,14 +77,21 @@ if(keys.right.pressed && player.position.x < 400) {
     player.velocity.x = 0
 
   if(keys.right.pressed) {
+    platforms.forEach((platform) => {
     platform.position.x -= 5
+})
   } else if(keys.left.pressed) {
-    platform.position.x = 5
+    platforms.forEach((platform) => {
+      platform.position.x = 5
+})
   }
 }
+  platforms.forEach((platform) => {
+      // platform.position.x = 5
   if(player.position.y + player.height <= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width) {
     player.velocity.y = 0
   }
+})
 }
 
 animate()
